7bcdc76ed8ace98ced2412b63031db53c387d117
fo0man <jmatty1983@gmail.com>
2015-02-13 04:38:52 +0000

    Finish user log-in and sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      log_in @user
+      redirect_back_or @user
+    else
+      render 'new'
+    end
+  end
+  
   def show
   end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,13 @@
 module SessionsHelper
   
+  def log_in(user)
+    session[:id] = user.id  
+  end
+  
+  def log_out
+    session[:id] = nil  
+  end
+  
   def redirect_back_or(default)
     redirect_to(session[:forwarding_url] || default)
     session.delete(:forwarding_url)
